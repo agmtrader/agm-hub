728705de3bfff7f00489f2fd6b37b2cb665b462f
Andres Aguilar <aguilarcarboni@gmail.com>
2024-07-01 16:13:58 -0600

    Updae

---
+++ src/lib/auth.ts
@@ -0,0 +1,75 @@
+import { adminAuth, firestoreAdmin } from "@/utils/firebase-admin"
+import { FirestoreAdapter } from "@next-auth/firebase-adapter"
+import { NextAuthOptions } from "next-auth"
+
+import GoogleProvider from 'next-auth/providers/google'
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_ID!,
+        clientSecret: process.env.GOOGLE_SECRET!,
+        checks: 'none',
+        authorization: {
+          params: {
+            prompt: "consent",
+            access_type: "offline",
+            response_type: "code",
+            scope: "openid https://www.googleapis.com/auth/drive"
+          }
+        }
+      }),
+    ],
+    adapter: FirestoreAdapter(firestoreAdmin),
+    callbacks: {
+      jwt: async ({ token, user, account }) => {
+
+        if (user) {
+          token.sub = user.id
+          
+        }
+        
+        if (account) {
+          token.accessToken = account.access_token
+          token.refreshToken = account.refresh_token
+        }
+        
+        return token
+      },
+      async session({ session, token }) {
+        if (session?.user) {
+
+          if (token.sub) {
+
+            session.user.id = token.sub
+  
+            /*
+            const options = {
+              admin: false
+            }
+  
+            if (session.user.email?.split('@')[1] == 'agmtechnology.com' || authedUsers.includes(session.user.email)) {
+              options.admin = true
+            }
+            */
+  
+            const firebaseToken = await adminAuth.createCustomToken(token.sub)
+            session.firebaseToken = firebaseToken
+
+          }
+
+          if (token.accessToken && token.refreshToken) {
+
+            session.user.accessToken = token.accessToken
+            session.user.refreshToken = token.refreshToken
+
+          }
+          
+        }
+        return session
+      },
+    },
+    session: {
+      strategy: 'jwt'
+    },
+}
