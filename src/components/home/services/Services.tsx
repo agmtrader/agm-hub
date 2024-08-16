19c8ed532ea67a776484bb094937f26836f66a38
Andres Aguilar <aguilarcarboni@gmail.com>
2024-08-16 14:30:08 -0600

    .

--- src/components/home/services/Services.tsx
+++ src/components/home/services/Services.tsx
@@ -5,15 +5,15 @@ import { GraphUpArrow, Mortarboard } from 'react-bootstrap-icons'
 const services = [
   {
     name: 'AGM Trader',
-    icon: <GraphUpArrow className='text-white h-[15vw] w-[15vw]'/>
+    icon: <GraphUpArrow className='text-white h-[12vw] w-[12vw]'/>
   },
   {
     name: 'AGM Advisor',
-    icon: <Mortarboard className='text-white h-[15vw] w-[15vw]'/>
+    icon: <Mortarboard className='text-white h-[12vw] w-[12vw]'/>
   },
   {
     name: 'AGM Institutional',
-    icon: <Handshake className='text-white h-[15vw] w-[15vw]'/>
+    icon: <Handshake className='text-white h-[12vw] w-[12vw]'/>
   }
 ]
 
@@ -22,10 +22,10 @@ function Services() {
     <div className='flex flex-col h-full w-full'>
       <div className='bg-agm-blue w-full h-full justify-center items-center flex flex-col gap-y-16 py-20'>
         <h2 className='text-agm-white text-5xl font-bold'>Our Services</h2>
-        
+      
         <div className='flex items-center justify-evenly w-full'>
           {services.map((element, index) => (
-              <div key={index} className='w-fit h-full gap-y-10 flex flex-col justify-center items-center rounded-3xl p-[5vw] bg-agm-dark-blue'>
+              <div key={index} className='w-fit h-fit p-6 flex justify-center items-center rounded-2xl bg-agm-dark-blue'>
                 {element.icon}
               </div>
           ))}
