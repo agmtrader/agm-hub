ff4ffbae81438a1d181c234c21cee53b539422b4
Andrés Aguilar <aguilarcarboni@gmail.com>
2025-02-18 11:39:04 -0600

    Update firebase

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -5,12 +5,12 @@ import { getApp, getApps, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID
+    apiKey: "AIzaSyADSCcUmflApv3U1BDZnskcbgdBXXLAk1A",
+    authDomain: "agm-datalake.firebaseapp.com",
+    projectId: "agm-datalake",
+    storageBucket: "agm-datalake.appspot.com",
+    messagingSenderId: "571127175324",
+    appId: "1:571127175324:web:02105fe799b088e6bab559"
 };
 
 // Initialize Firebase
