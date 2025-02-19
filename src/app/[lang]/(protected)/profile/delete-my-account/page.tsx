0f02bb90000096f642b60e786710f326e45bdb49
Andrés Aguilar <aguilarcarboni@gmail.com>
2025-02-19 11:05:45 -0600

    Add delete account page

---
+++ src/app/[lang]/(protected)/profile/delete-my-account/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+const page = () => {
+
+  return (
+    <div className='w-full h-full gap-y-5 flex flex-col justify-center items-center'>
+    </div>
+  )
+}
+
+export default page
