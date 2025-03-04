e37dd2de429ed7bee94b07b1b32e21f7e635d20d
Andres Aguilar <aguilarcarboni@gmail.com>
2025-03-04 14:38:00 -0600

    Add firestore backup reader

---
+++ src/utils/firestore.ts
@@ -0,0 +1,32 @@
+'use server'
+import { firestoreAdmin } from '@/utils/firebase-admin'
+
+export async function getRecoveryData() {
+    try {
+        const userRef = firestoreAdmin.collection('users').doc('20250123124721')
+        
+        const result = await firestoreAdmin.runTransaction(async (transaction) => {
+            const doc = await transaction.get(userRef)
+            if (!doc.exists) {
+                throw new Error('User document does not exist!')
+            }
+
+            // Get the data from the document
+            const userData = doc.data()
+            
+            // You can perform additional operations within the transaction here
+            // For example, updating a lastAccessed timestamp:
+            // transaction.update(userRef, {
+            //     lastAccessed: Timestamp.now()
+            // })
+
+            return userData
+        })
+        console.log(result)
+
+        return result
+    } catch (error) {
+        console.error('Error getting recovery data:', error)
+        throw error
+    }
+}
