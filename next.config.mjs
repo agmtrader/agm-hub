9e5035ee4fe4dab3629e66e77cbec4829dc8a585
Andres Aguilar <aguilarcarboni@gmail.com>
2025-03-24 14:15:50 -0600

    Add firebase envs

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,9 @@ const nextConfig = {
         NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
         GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
         GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+        FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+        FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
+        FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY,
         DEV_MODE: process.env.DEV_MODE
     },
     images: {
