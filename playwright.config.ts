ccfd8bb81262c4a6021e0dc51c095a35885b684c
Andres Aguilar <aguilarcarboni@gmail.com>
2025-04-04 09:46:32 -0600

    Push browser tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,14 +16,7 @@ export default defineConfig({
       name: 'webkit',
       use: { 
         ...devices['Desktop Safari'],
-        contextOptions: {
-          userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15'
-        }
       },
-    },
-    {
-      name: 'webkit-mobile',
-      use: { ...devices['iPhone 12'] },
     },
     {
       name: 'chromium',
