f8c208142304ce8e7b8e033f3af782d31b7e98c4
Andres Aguilar <aa@agmtechnology.com>
2025-12-09 13:37:13 -0600

    Update default values in IBKRApplicationForm to use individual_form for improved application handling

--- src/components/apply/form/IBKRApplicationForm.tsx
+++ src/components/apply/form/IBKRApplicationForm.tsx
@@ -67,7 +67,7 @@ const IBKRApplicationForm = () => {
 
   const form = useForm<Application>({
     resolver: zodResolver(application_schema),
-    defaultValues: getApplicationDefaults(application_schema),
+    defaultValues: individual_form,
     mode: 'onChange',
     shouldUnregister: false,
   });
