2d66e8ea2633b48d7d145aab06effd9876411279
Andres Aguilar <aguilarcarboni@gmail.com>
2026-05-26 10:13:29 -0600

    refactor: remove TempPage component and its associated TempBannerMockup import

--- src/app/[lang]/(public)/temp/page.tsx
+++
@@ -1,9 +0,0 @@
-import TempBannerMockup from "@/components/hub/main/temp/TempBannerMockup"
-
-export default function TempPage() {
-  return (
-    <main className="min-h-[70vh]">
-      <TempBannerMockup />
-    </main>
-  )
-}
